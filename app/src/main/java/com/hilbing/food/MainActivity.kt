f82ec9460c590f16bb36e51be9d11c9a7ed3e131
ghilbing <ghilbing@gmail.com>
2021-05-17 16:25:37 -0700

    Fragments and Bottom Navigation Menu

--- app/src/main/java/com/hilbing/food/MainActivity.kt
+++ app/src/main/java/com/hilbing/food/MainActivity.kt
@@ -2,10 +2,38 @@ package com.hilbing.food
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.hilbing.food.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityMainBinding
+
+    private lateinit var mNavController: NavController
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        mNavController = findNavController(R.id.nav_host_fragment_container)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.recipesFragment,
+            R.id.favoriteRecipesFragment,
+            R.id.foodJokeFragment))
+        setupActionBarWithNavController(mNavController, appBarConfiguration)
+        mBinding.bottomNavigationView.setupWithNavController(mNavController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(mNavController, null)
     }
 }
