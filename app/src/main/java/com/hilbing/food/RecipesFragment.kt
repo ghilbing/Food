31ca968d928132e1fe898c5d26e414a6202443dd
ghilbing <ghilbing@gmail.com>
2021-05-19 13:56:35 -0700

    Placeholder layout

--- app/src/main/java/com/hilbing/food/RecipesFragment.kt
+++ app/src/main/java/com/hilbing/food/RecipesFragment.kt
@@ -5,15 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hilbing.food.databinding.ActivityMainBinding
+import com.hilbing.food.databinding.FragmentRecipesBinding
 
 class RecipesFragment : Fragment() {
+
+    private lateinit var mBinding: FragmentRecipesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        mBinding = FragmentRecipesBinding.inflate(inflater, container, false)
+        return mBinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mBinding.recyclerview.showShimmer()
     }
 
 }
