ec03b1b8fa34c4311f9a73dca9e2fac1b8d2a986
ghilbing <ghilbing@gmail.com>
2021-05-20 16:10:55 -0700

    Inject RemoteDataSource

--- app/src/main/java/com/hilbing/food/api/RemoteDataSource.kt
+++ app/src/main/java/com/hilbing/food/api/RemoteDataSource.kt
@@ -1,4 +1,13 @@
 package com.hilbing.food.api
 
-class RemoteDataSource() {
+import com.hilbing.food.model.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipeApi: FoodRecipeApi
+) {
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe>{
+        return foodRecipeApi.getRecipes(queries)
+    }
 }
